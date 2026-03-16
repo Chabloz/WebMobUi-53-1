adeecdcdcceb76299d9c2a23dc83249c2deace5b
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2026-03-16 11:48:15 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+  import { ref, watch } from 'vue';
+  import TheHeader from './components/TheHeader.vue';
+
+  const username = defineModel('username');
+
+  setTimeout(() => {
+    username.value = 'new username';
+  }, 5000);
+
+  watch(username, (newValue) => {
+     if (newValue === 'admin') {
+        username.value = '';
+     }
+  });
+
+  // const age = ref(0);
+  // age.value += 2;
+  // console.log(age.value);
+  // setTimeout(() => {
+  //   age.value += 2;
+  //   console.log(age.value);
+  // }, 6000);
+</script>
+
+<template>
+  <h1>main title</h1>
+  <TheHeader />
+
+  <div>
+    <input v-model="username" type="email">
+    {{  username }}
+  </div>
+  <!-- <the-place>the place</the-place> -->
+  <!-- <div>{{ age }}</div> -->
+</template>
+
+<style>
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { ref } from 'vue';
+  import BaseButton from './BaseButton.vue';
+  const label = ref('dark');
+
+  function toggleTheme() {
+    label.value = label.value === 'dark' ? 'light' : 'dark';
+  }
+</script>
+
+<template>
+  <header>
+    <h1>IM Webmobui</h1>
+    <BaseButton @click="toggleTheme" :label="label" type="primary"/>
+  </header>
+</template>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
