15760d2da8b2cfc3fad25cbb3b7a049cfc2a05e7
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2026-03-30 11:40:03 +0200

    Collection reactive

--- src/pages/Schedule.vue
+++ src/pages/Schedule.vue
@@ -2,23 +2,22 @@
   import { compile, computed } from 'vue';
 import { useFetch } from '../composables/useFetch';
 
-  const {data, error, loading} = useFetch('https://chabloz.eu/files/horaires/all'); // hard coder
+  const {data, error, loading} = useFetch('https://chabloz.eu/files/horaires/all');
 
   const filter = defineModel();
 
   const schedulFiltered = computed(() => {
-    return data.value.map((c) => c.label.search(filter.value) != -1);
+    return data.value.filter((c) => c.label.search(filter.value) != -1);
   })
 
   const scheduleSorted = computed(() => {
     if (!schedulFiltered.value) return [];
     const sorted = schedulFiltered.value.toSorted((c1, c2) => {
+      console.log(c1, c2);
       return c1.start.localeCompare(c2.start);
     })
     return sorted;
   })
-
-  // const {data, error, loading} = useFetch('https://example.com'); // hard coder
 </script>
 
 <template>
@@ -30,7 +29,7 @@ import { useFetch } from '../composables/useFetch';
     <div v-if="data">
       <input v-model="filter" type="text">
       {{  filter  }}
-      <div v-for="cours in schedulFiltered">
+      <div v-for="cours in scheduleSorted">
         {{  cours.label }}
         {{  cours.start }}
       </div>
